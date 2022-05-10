317f7615dbf5b4ea8339944f822292273d3ae711
kikiAgustin <kikiagustin62@gmail.com>
2022-05-10 06:40:05 +0600

    tambah container dan form input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { useState } from 'react';
+import './App.css';
+import shoppingIcon from './assets/shopping-icon.svg';
 
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => { setCount(count + 1) }} >Plus</button>
-      <button onClick={() => { setCount(count - 1) }} >Minus</button>
-    </div>
+    <>
+      <nav className="nav" >
+        <img className="nav-icon" src={shoppingIcon} alt="Shopping Icon" />
+        <h1 className="nav-title" >Shopping List</h1>
+      </nav>
+
+      <section className="container" >
+        <form className="form" >
+          <input className="input" type="text" placeholder="List" />
+          <button className="add-button" type="submit" >Add</button>
+        </form>
+      </section>
+    </>
   );
 }
 
