0769c8bf5fceca5386cde6877b4247648e2fe74a
kikiAgustin <kikiagustin62@gmail.com>
2022-05-12 06:40:02 +0600

    menjalankan add input dan handle

--- src/App.js
+++ src/App.js
@@ -14,6 +14,36 @@ function App() {
     { title: 'Semangkat', count: 1 }
 
   ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert('No Blank List');
+      return
+    }
+
+    const addedTodos = [...todos, {
+      title: value,
+      count: 1
+    }];
+
+    setTodos(addedTodos);
+    setValue('');
+  }
+
+  const handleAdditionCount = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].count = newTodos[index].count + 1;
+    setTodos(newTodos);
+  }
+
+  const handleSubtractionCount = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].count = newTodos[index].count - 1;
+    setTodos(newTodos);
+  }
+
   return (
     <>
       <nav className="nav" >
@@ -22,7 +52,7 @@ function App() {
       </nav>
 
       <section className="container" >
-        <form className="form" >
+        <form className="form" onSubmit={handleSubmit} >
           <input
             onChange={(e) => { setValue(e.target.value) }}
             value={value}
@@ -42,11 +72,11 @@ function App() {
                   <div className="todo-icon-wrapper" >
                     <div className='todo-count' > {todo.count} </div>
 
-                    <button className='todo-action-button' >
+                    <button onClick={() => handleSubtractionCount(index)} className='todo-action-button' >
                       <img src={minusIcon} alt="Minus Icon" />
                     </button>
 
-                    <button className='todo-action-button' >
+                    <button onClick={() => handleAdditionCount(index)} className='todo-action-button' >
                       <img src={plusIcon} alt="Plus Icon" />
                     </button>
 
